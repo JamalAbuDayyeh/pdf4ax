67392e07d51fe41226a3175e3f190b0e17e10021
ku@digitaldolphins.jp <ku@digitaldolphins.jp@18802ecc-be5d-c5ca-c107-146387257ce5>
2011-09-03 14:22:39 +0000

    pixmanとcairoを追加した。

--- trunk/PDF4Ax/PDF4Axidl.h
+++ trunk/PDF4Ax/PDF4Axidl.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Aug 26 15:01:01 2011
+/* at Sat Sep 03 23:19:52 2011
  */
 /* Compiler settings for .\PDF4Ax.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
